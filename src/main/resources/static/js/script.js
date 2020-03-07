b47f9fee58dc1ff57ed62ac02f6e4cefce7a3a6a
demosocket <demosocket.@gmail.com>
2020-03-07 18:06:51 +0300

    changed ship-div and list

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,15 +3,14 @@ ourRequest.open('GET', 'http://localhost:8080/ships/all');
 ourRequest.onload = function () {
     var ourShips = JSON.parse(ourRequest.responseText);
     ourShips.forEach(element => console.log(element));
-    var resultTable = '<table class="ship-table">' +
-        '<tr><td>Ship</td><td>Manufacturer</td><td>Cost, cr</td><td>Landing Pad</td></tr>';
-    ourShips.forEach(ship => resultTable+='<tr>' +
-        '<td>'+ ship.shipModel + '</td>' +
-        '<td>'+ ship.manufacturer + '</td>' +
-        '<td>'+ ship.cost + '</td>' +
-        '<td>'+ ship.size + '</td>' +
-        '</tr>');
-    resultTable+='</table>';
-    document.getElementById("demo").innerHTML = resultTable;
+    var resultDiv = '<div>';
+    ourShips.forEach(ship => resultDiv += '<div class="ship-div">' +
+        '<p> Model: ' + ship.shipModel + '</p>' +
+        '<p> Manufacturer: ' + ship.manufacturer + '</p>' +
+        '<p> Cost: ' + ship.cost + ' cr</p>' +
+        '<p> Landing Pad: ' + ship.size + '</p>' +
+        '</div>');
+    resultDiv += '</div>';
+    document.getElementById("placeholder").innerHTML = resultDiv;
 };
 ourRequest.send();
