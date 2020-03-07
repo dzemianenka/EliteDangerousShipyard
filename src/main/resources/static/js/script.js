f0bab391670f99cbafb6f59c6ab577eaf988d781
demosocket <demosocket.@gmail.com>
2020-03-07 17:03:15 +0300

    plus ship-list

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,17 @@
-const ourRequest = new XMLHttpRequest();
+var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'http://localhost:8080/ships/all');
 ourRequest.onload = function () {
-    const ourShips = JSON.parse(ourRequest.responseText);
-    console.log(ourShips[0]);
+    var ourShips = JSON.parse(ourRequest.responseText);
+    ourShips.forEach(element => console.log(element));
+    var resultTable = '<table class="ship-table">' +
+        '<tr><td>Ship</td><td>Manufacturer</td><td>Cost, cr</td><td>Landing Pad</td></tr>';
+    ourShips.forEach(ship => resultTable+='<tr>' +
+        '<td>'+ ship.shipModel + '</td>' +
+        '<td>'+ ship.manufacturer + '</td>' +
+        '<td>'+ ship.cost + '</td>' +
+        '<td>'+ ship.size + '</td>' +
+        '</tr>');
+    resultTable+='</table>';
+    document.getElementById("demo").innerHTML = resultTable;
 };
 ourRequest.send();
