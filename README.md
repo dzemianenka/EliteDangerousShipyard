dfc890c0854daeb842eafcfc7b0611f3fb58b637
demosocket <demosocket.@gmail.com>
2020-03-28 11:47:52 +0300

    change generated and readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Elite Dangerous web app contains ships and search filter. 
 Database queries are executed **without** a full page reload with javascript.
 ***
-The project is built on Spring, Hibernate, Javascript and template engine Thymeleaf.
+The project is built on Spring, Hibernate, JavaScript and template engine Thymeleaf.
 
 ![Image alt](https://github.com/Demosocket/EliteDangerousShipyard/blob/master/screenshots/StartPage.png)
 ![Image alt](https://github.com/Demosocket/EliteDangerousShipyard/blob/master/screenshots/SearchQuery.png)
--- src/main/java/com/demosocket/shipyard/model/AjaxBody.java
+++ src/main/java/com/demosocket/shipyard/model/AjaxBody.java
@@ -3,20 +3,46 @@ package com.demosocket.shipyard.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
-public class AjaxBody {
+public class AjaxBody{
 
+    @NotNull
+    @Min(value = 0, message = "priceMin should not be less than 0")
     private Integer priceMin;
+
+    @NotNull
+    @Min(value = 31000, message = "priceMax should not be less than 31000")
     private Integer priceMax;
+
+    @NotNull
     private Boolean coreDynamics;
+
+    @NotNull
     private Boolean faulconDeLacy;
+
+    @NotNull
     private Boolean gutamaya;
+
+    @NotNull
     private Boolean lakon;
+
+    @NotNull
     private Boolean saudKruger;
+
+    @NotNull
     private Boolean zorgonPeterson;
+
+    @NotNull
     private Boolean large;
+
+    @NotNull
     private Boolean medium;
+
+    @NotNull
     private Boolean small;
 
 }
